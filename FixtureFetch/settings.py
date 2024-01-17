111a6c8ac2e9b50e902e695415f356e7212cb394
ronanwill97 <ronan.williams22@gmail.com>
2024-01-17 14:01:48 +0000

    Update settings.py

--- FixtureFetch/settings.py
+++ FixtureFetch/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)j&*t3i76gtcn_wh(!a4%udj3nf#s9i$b=-b6g*^6bz(ztotq_'
+SECRET_KEY = os.environ.get("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get("APP_DOMAIN")]
 
 # Application definition
 
